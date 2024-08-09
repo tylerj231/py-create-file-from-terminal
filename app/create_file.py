0aa3c6e31ca98d8a9371794925d6e28dabb85f91
Mykola <jacktyler2391@gmail.com>
2024-08-09 18:22:01 +0300

    Solution

--- app/create_file.py
+++ app/create_file.py
@@ -1,1 +1,18 @@
-# write your code here
+import argparse
+import os
+from utils import create_file_only, create_directory_only
+
+
+parser = argparse.ArgumentParser(description="script to create files")
+parser.add_argument("-d", nargs="+")
+parser.add_argument("-f")
+args = parser.parse_args()
+
+
+if args.d is not None and args.f is not None:
+    path = create_directory_only(args.d)
+    create_file_only(os.path.join(path, args.f))
+elif args.d is not None:
+    create_directory_only(args.d)
+elif args.f is not None:
+    create_file_only(args.f)
---
+++ app/utils.py
@@ -0,0 +1,25 @@
+import os
+import sys
+from datetime import datetime
+
+arguments = sys.argv
+
+
+def create_directory_only(folders: list) -> str:
+    path = "/".join([d for d in folders])
+    os.makedirs(path, exist_ok=True)
+    return path
+
+
+def create_file_only(file_name: str) -> None:
+    with open(file_name, "a") as file:
+        current_date = datetime.now()
+        file.write(current_date.strftime("%Y-%m-%d %H:%M:%S\n"))
+        number_line = 0
+        while True:
+            file_content = input("Enter content line: ")
+            if file_content == "stop":
+                break
+
+            number_line += 1
+            file.write(f"{number_line} {file_content}\n")
